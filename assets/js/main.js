2f87c7a5e5986c23f2df295f20654c78a655dfed
Hema-venkat3 <hemavenkatesan3@gmail.com>
2024-01-26 15:15:47 +0530

    completed design part

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// CUSTOMER DETAILS
+const nameEl = document.querySelector("#Name");
+const addressEl = document.querySelector("#address");
+const numberEl = document.querySelector("#number");
+const emailEl = document.querySelector("#email");
+const buttonEl = document.querySelector("#button");
+
+// VALIDATIONS
+if(nameEl){
+    
+}
